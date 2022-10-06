8c7e1ce3fd8bf48b705c5f024f16a9d24fa0e677
Herb Susmann <herbps10@gmail.com>
2022-10-06 17:49:52 -0400

    Updated targets with logistic model

--- R/fpem_cv.R
+++ R/fpem_cv.R
@@ -1,34 +1,61 @@
-cv_fit <- function(data, held_out, ...) {
-  fpemplus(
-      data,
-      y = "contraceptive_use_modern",
-      se = "se_modern",
-      year = "year",
-      source = "data_series_type",
-      area = "name_country",
-      held_out = held_out, 
-      
-      start_year = 1970, 
-      end_year = 2030, 
-      
-      # Hierarchical setup
-      hierarchical_level     = c("intercept", "name_region", "name_sub_region", "name_country"), 
-      hierarchical_splines   = c("intercept", "name_region", "name_sub_region", "name_country"),
-      hierarchical_asymptote = c("intercept", "name_region", "name_sub_region", "name_country"),
-      
-      # Stan sampler settings
-      parallel_chains = 4,
-      refresh = 50, 
-      ...
-    )
+cv_fit <- function(data, held_out, logistic = FALSE, ...) {
+  if(logistic == TRUE) {
+    fpemplus_logistic(
+        data,
+        y = "contraceptive_use_modern",
+        se = "se_modern",
+        year = "year",
+        source = "data_series_type",
+        area = "name_country",
+        held_out = held_out, 
+        
+        start_year = 1970, 
+        end_year = 2030, 
+        
+        # Hierarchical setup
+        hierarchical_level     = c("intercept", "name_region", "name_sub_region", "name_country"), 
+        hierarchical_splines   = c("intercept", "name_region", "name_sub_region", "name_country"),
+        hierarchical_asymptote = c("intercept", "name_country"),
+        hierarchical_rate      = c("intercept", "name_region", "name_sub_region", "name_country"),
+        
+        # Stan sampler settings
+        parallel_chains = 4,
+        refresh = 50, 
+        ...
+      )
+  }
+  else {
+    fpemplus(
+        data,
+        y = "contraceptive_use_modern",
+        se = "se_modern",
+        year = "year",
+        source = "data_series_type",
+        area = "name_country",
+        held_out = held_out, 
+        
+        start_year = 1970, 
+        end_year = 2030, 
+        
+        # Hierarchical setup
+        hierarchical_level     = c("intercept", "name_region", "name_sub_region", "name_country"), 
+        hierarchical_splines   = c("intercept", "name_region", "name_sub_region", "name_country"),
+        hierarchical_asymptote = c("intercept", "name_country"),
+        
+        # Stan sampler settings
+        parallel_chains = 4,
+        refresh = 50, 
+        ...
+      )
+  }
 }
 
-cv_fit_cutoff <- function(data, cutoff, ...) {
+cv_fit_cutoff <- function(data, cutoff, logistic = FALSE, ...) {
   held_out <- data$year >= cutoff
-  cv_fit(data, held_out, ...) 
+  cv_fit(data, held_out, logistic = logistic, ...) 
 }
 
-cv_fit_random <- function(data, seed, prop = 0.2, reps = 5, ...) {
+cv_fit_random <- function(data, seed, prop = 0.2, reps = 5, logistic = FALSE, ...) {
   set.seed(seed)
   tibble(
     rep = 1:reps
@@ -40,6 +67,6 @@ cv_fit_random <- function(data, seed, prop = 0.2, reps = 5, ...) {
       held_out
     })) %>%
     mutate(cv_fit = map(held_out, function(held_out) {
-      cv_fit(data, held_out, ...)
+      cv_fit(data, held_out, logistic = logistic, ...)
     }))
 }
--- _targets.R
+++ _targets.R
@@ -23,14 +23,24 @@ cv_max_treedepth    <- 12
 cv_iter_warmup      <- 250
 cv_iter_sampling    <- 500
 
+#print("TESTING MODE")
+#cv_adapt_delta      <- 0.95
+#cv_max_treedepth    <- 8
+#cv_iter_warmup      <- 250
+#cv_iter_sampling    <- 250
+
+
 final_adapt_delta   <- 0.999
 final_max_treedepth <- 14
 final_iter_warmup   <- 500
 final_iter_sampling <- 500
 
 output_dir <- "/work/hsusmann_umass_edu/spline_rate_model_paper/output/"
+#output_dir <- "d:/spline_rate_model_paper/output"
 
 countries <- c()
+#countries <- c("Somalia", "India", "Palau", "Bangladesh", "Zimbabwe",
+#             "Indonesia", "Kenya", "Turkey", "Mexico", "Guatemala")
 
 analysis_data_target <- tar_target(analysis_data, national_data(
   countries = countries,
@@ -46,11 +56,12 @@ analysis_data_target <- tar_target(analysis_data, national_data(
 )
 
 cv_setup <- tribble(
-  ~model_name, ~spline_degree, ~num_knots,
-  "spline",    2,              5,
-  "spline",    2,              7,
-  "spline",    3,              5,
-  "spline",    3,              7
+  ~model,     ~spline_degree, ~num_knots,
+  "spline",   2,              5,
+  "spline",   2,              7,
+  "spline",   3,              5,
+  "spline",   3,              7,
+  "logistic", 2,              7
 )
 
 cv_fits_target <- tar_map(
@@ -59,8 +70,8 @@ cv_fits_target <- tar_map(
   tar_target(cv_fit_cutoff2010, cv_fit_cutoff(
     data = analysis_data,
     cutoff = 2010,
+    logistic = model == "logistic",
     
-    model = model_name,
     spline_degree = spline_degree,
     num_knots = num_knots,
     
@@ -71,13 +82,13 @@ cv_fits_target <- tar_map(
     max_treedepth = cv_max_treedepth,
     iter_warmup   = cv_iter_warmup,
     iter_sampling = cv_iter_sampling,
-    seed = ifelse(model_name == "spline", 100, 0) + spline_degree + num_knots
+    seed = ifelse(model == "logistic", 100, 0) + spline_degree + num_knots
   )),
   tar_target(cv_fit_cutoff2015, cv_fit_cutoff(
     data = analysis_data,
     cutoff = 2015,
+    logistic = model == "logistic",
     
-    model = model_name,
     spline_degree = spline_degree,
     num_knots = num_knots,
     
@@ -89,26 +100,25 @@ cv_fits_target <- tar_map(
     iter_warmup   = cv_iter_warmup,
     iter_sampling = cv_iter_sampling,
 		output_dir = output_dir,
-    seed = ifelse(model_name == "spline", 100, 0) + spline_degree + num_knots
+    seed = ifelse(model == "logistic", 150, 0) + spline_degree + num_knots
+  )),
+  tar_target(cv_fit_random5, cv_fit_random(
+    data = analysis_data,
+    seed = ifelse(model == "logistic", 200, 0) + spline_degree + num_knots,
+    prop = 0.2,
+    reps = 2,
+    
+    spline_degree = spline_degree,
+    num_knots = num_knots,
+    
+    tau_prior = tau_prior,
+    rho_prior = rho_prior,
+    
+    adapt_delta   = cv_adapt_delta,
+    max_treedepth = cv_max_treedepth,
+    iter_warmup   = cv_iter_warmup,
+    iter_sampling = cv_iter_sampling,
   ))
-  #tar_target(cv_fit_random5, cv_fit_random(
-  #  data = analysis_data,
-  #  seed = ifelse(model_name == "spline", 200, 0) + spline_degree + num_knots,
-  #  prop = 0.2,
-  #  reps = 5,
-  #  
-  #  model = model_name,
-  #  spline_degree = spline_degree,
-  #  num_knots = num_knots,
-  #  
-  #  tau_prior = tau_prior,
-  #  rho_prior = rho_prior,
-  #  
-  #  adapt_delta   = cv_adapt_delta,
-  #  max_treedepth = cv_max_treedepth,
-  #  iter_warmup   = cv_iter_warmup,
-  #  iter_sampling = cv_iter_sampling,
-  #))
 )
 
 final_spline_target <- tar_target(final_spline, fpemplus(
@@ -131,7 +141,7 @@ final_spline_target <- tar_target(final_spline, fpemplus(
   # Hierarchical setup
   hierarchical_level     = c("intercept", "name_region", "name_sub_region", "name_country"), 
   hierarchical_splines   = c("intercept", "name_region", "name_sub_region", "name_country"),
-  hierarchical_asymptote = c("intercept", "name_region", "name_sub_region", "name_country"),
+  hierarchical_asymptote = c("intercept", "name_country"),
   
   # Prior settings
   tau_prior = "normal(0, 2)",
@@ -143,6 +153,44 @@ final_spline_target <- tar_target(final_spline, fpemplus(
   iter_warmup = final_iter_warmup,
   iter_sampling = final_iter_sampling,
 	output_dir = output_dir,
+  seed = 1482395,
+  parallel_chains = 4,
+  refresh = 50
+))
+
+final_logistic_target <- tar_target(final_logistic, fpemplus_logistic(
+  analysis_data,
+  y = "contraceptive_use_modern",
+  se = "se_modern",
+  year = "year",
+  source = "data_series_type",
+  area = "name_country",
+  
+  start_year = 1970, 
+  end_year = 2030, 
+  
+  model = "spline",
+  
+  # Spline setup
+  spline_degree = 2,
+  num_knots = 5,
+  
+  # Hierarchical setup
+  hierarchical_level     = c("intercept", "name_region", "name_sub_region", "name_country"), 
+  hierarchical_splines   = c("intercept", "name_region", "name_sub_region", "name_country"),
+  hierarchical_rate      = c("intercept", "name_region", "name_sub_region", "name_country"),
+  hierarchical_asymptote = c("intercept", "name_country"),
+  
+  # Prior settings
+  tau_prior = "normal(0, 2)",
+  rho_prior = "uniform(0, 1)",
+  
+  # Stan sampler settings
+  adapt_delta = final_adapt_delta,
+  max_treedepth = final_max_treedepth,
+  iter_warmup = final_iter_warmup,
+  iter_sampling = final_iter_sampling,
+  output_dir = output_dir,
   seed = 1482395,
   parallel_chains = 4,
   refresh = 50
